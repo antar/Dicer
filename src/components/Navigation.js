7cbca02c1a56cfff9c85cab7a3df6cf6b63e35d0
Alessio Carcavallo <toobrokeforrysll@gmail.com>
2022-02-16 15:02:48 +0100

    feat: better nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,6 @@ export default function Navigation() {
     return (
         <nav className="navigation">
             <h1 className="navigation-title">Dicer</h1>
-            <h4 className="navigation-profile">antar</h4>
         </nav>
     )
 }
